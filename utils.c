8170fd60256945c0b00e09ee990bc466e970ff50
lrjussiau <lr.jussiaume@gmail.com>
2024-01-24 11:13:51 +0100

    added Struct to the program

--- utils.c
+++ utils.c
@@ -1,31 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ljussiau <ljussiau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/24 10:53:00 by ljussiau          #+#    #+#             */
-/*   Updated: 2024/01/24 11:01:59 by ljussiau         ###   ########.fr       */
+/*   Created: 2024/01/24 10:54:23 by ljussiau          #+#    #+#             */
+/*   Updated: 2024/01/24 10:55:57 by ljussiau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int	main(int ac, char **av)
+void	error(char *str)
 {
-	t_data	data;
-
-	if (ac == 5 || ac == 6)
-	{
-		parsing(&data, av);
-		init(&data);
-		simulation(&data);
-		end_simulation(&data);
-	}
-	else
-	{
-		ft_error("Wrong Input.");
-	}
-	return (0);
+	printf("Error : %s\n", str);
+	exit (EXIT_FAILURE);
 }
